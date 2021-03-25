a86aecb83fef8df472b44294696e98607669d678
Sumit Kumar <47416945+ssumitk14@users.noreply.github.com>
2021-03-25 18:58:10 +0530

    Added main

--- Project.py
+++ Project.py
@@ -55,61 +55,63 @@ def saveToFile():
 
 
 
+if __name__ == "__main__":
+    
+    
+    global root = Tk()
+    root.geometry("270x300")
 
-root = Tk()
-root.geometry("270x300")
+    firstName = Label(root, text='First Name: ', fg='violet')
+    firstName.grid(row=0,sticky='E')
+    lastName = Label(root,text='Last Name: ', fg='indigo')
+    lastName.grid(row=1,sticky='E')
+    birth = Label(root,text='Date of Birth:\n(dd/mm/yyyy)', fg='blue')
+    birth.grid(row=3,sticky='E')
+    email = Label(root,text='Email: ',fg='green')
+    email.grid(row=4,sticky='E')
+    phone = Label(root, text='Phone No: ', fg='magenta')
+    phone.grid(row=5,sticky='E')
+    city = Label(root, text='City: ',fg='cyan')
+    city.grid(row=6,sticky=E)
+    state = Label(root, text='State: ', fg='brown')
+    state.grid(row=7,sticky=E)
+    zip = Label(root,text='Zip Code: ',fg='olive')
+    zip.grid(row=8,sticky=E)
 
-firstName = Label(root, text='First Name: ', fg='violet')
-firstName.grid(row=0,sticky='E')
-lastName = Label(root,text='Last Name: ', fg='indigo')
-lastName.grid(row=1,sticky='E')
-birth = Label(root,text='Date of Birth:\n(dd/mm/yyyy)', fg='blue')
-birth.grid(row=3,sticky='E')
-email = Label(root,text='Email: ',fg='green')
-email.grid(row=4,sticky='E')
-phone = Label(root, text='Phone No: ', fg='magenta')
-phone.grid(row=5,sticky='E')
-city = Label(root, text='City: ',fg='cyan')
-city.grid(row=6,sticky=E)
-state = Label(root, text='State: ', fg='brown')
-state.grid(row=7,sticky=E)
-zip = Label(root,text='Zip Code: ',fg='olive')
-zip.grid(row=8,sticky=E)
+    firstNameEntry = Entry()
+    lastNameEntry = Entry()
+    birth = Entry()
+    email = Entry()
+    phone = Entry()
+    city = Entry()
+    state = Entry()
+    zip = Entry()
 
-firstNameEntry = Entry()
-lastNameEntry = Entry()
-birth = Entry()
-email = Entry()
-phone = Entry()
-city = Entry()
-state = Entry()
-zip = Entry()
+    firstNameEntry.grid(row=0,column=1)
+    lastNameEntry.grid(row=1,column=1)
+    birth.grid(row=3,column=1)
+    email.grid(row=4,column=1)
+    phone.grid(row=5,column=1)
+    city.grid(row=6,column=1)
+    state.grid(row=7,column=1)
+    zip.grid(row=8,column=1)
 
-firstNameEntry.grid(row=0,column=1)
-lastNameEntry.grid(row=1,column=1)
-birth.grid(row=3,column=1)
-email.grid(row=4,column=1)
-phone.grid(row=5,column=1)
-city.grid(row=6,column=1)
-state.grid(row=7,column=1)
-zip.grid(row=8,column=1)
-
-gender = IntVar()
-male = Radiobutton(root, text='Male', variable=gender,value=1)
-female = Radiobutton(root, text='Female', variable=gender,value=2)
-no = Radiobutton(root, variable=gender,value=3)
-
-
-
-male.place(x=58,y=42)
-female.grid(row=2,column=1)
-
-submitButton = Button(root,text='Submit ',command=saveToFile,bg='orange',fg='black')
-submitButton.place(x=60,y=220,width=150)
-#submitButton.grid(row=8,column=1,ipadx=40,ipady=10)
-
-quitButton = Button(root, text='Quit',bg='orange',fg='black' ,command=root.quit)
-quitButton.place(x=60,y=260,width=150)
+    gender = IntVar()
+    male = Radiobutton(root, text='Male', variable=gender,value=1)
+    female = Radiobutton(root, text='Female', variable=gender,value=2)
+    no = Radiobutton(root, variable=gender,value=3)
 
 
-root.mainloop()
+
+    male.place(x=58,y=42)
+    female.grid(row=2,column=1)
+
+    submitButton = Button(root,text='Submit ',command=saveToFile,bg='orange',fg='black')
+    submitButton.place(x=60,y=220,width=150)
+    #submitButton.grid(row=8,column=1,ipadx=40,ipady=10)
+
+    quitButton = Button(root, text='Quit',bg='orange',fg='black' ,command=root.quit)
+    quitButton.place(x=60,y=260,width=150)
+
+
+    root.mainloop()
